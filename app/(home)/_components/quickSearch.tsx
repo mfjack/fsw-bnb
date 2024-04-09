a812869e940e2258630a880285339485b4b7f101
mfjack <ferreira.marlon@live.com>
2024-04-09 11:51:41 -0300

    feat: add quick search

--- app/(home)/_components/quickSearch.tsx
+++ app/(home)/_components/quickSearch.tsx
@@ -1,41 +1,78 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const QuickSearch = () => {
 	return (
-		<section className='container mx-auto mt-5 px-5'>
+		<div className='container mx-auto p-5'>
 			<div className='flex items-center'>
 				<div className='w-full h-[1px] bg-grayLighter'></div>
-				<h2 className='px-4 font-medium text-grayPrimary whitespace-nowrap'>Tente pesquisar por</h2>
+				<h2 className='px-5 font-medium text-grayPrimary whitespace-nowrap'>Tente pesquisar por</h2>
 				<div className='w-full h-[1px] bg-grayLighter'></div>
 			</div>
 
-			<div className='w-full mt-5 flex justify-between items-center'>
-				<Image
-					src='/hotel.svg'
-					alt='Hotel'
-					width={48}
-					height={48}
-				/>
-				<Image
-					src='/fazenda.svg'
-					alt='Fazenda'
-					width={48}
-					height={48}
-				/>
-				<Image
-					src='/chale.svg'
-					alt='Chalé'
-					width={48}
-					height={48}
-				/>
-				<Image
-					src='/pousada.svg'
-					alt='Pousada'
-					width={48}
-					height={48}
-				/>
+			<div className='flex w-full justify-between mt-5 lg:mt-10 lg:justify-center gap-40'>
+				<div className='flex flex-col items-center gap-1'>
+					<Link
+						href={`/trips/search?text=hotel`}
+						className='flex flex-col items-center hover:text-primary transition-all'
+					>
+						<Image
+							width={35}
+							height={35}
+							src='/hotel-icon.svg'
+							alt='Hotel'
+						/>
+						<p className='text-sm lg:text-base text-grayPrimary'>Hotel</p>
+					</Link>
+				</div>
+
+				<div className='flex flex-col items-center gap-1'>
+					<Link
+						href='/trips/search?text=fazenda'
+						className='flex flex-col items-center hover:text-primary transition-all'
+					>
+						<Image
+							width={35}
+							height={35}
+							src='/farm-icon.svg'
+							alt='Fazenda'
+						/>
+						<p className='text-sm lg:text-base text-grayPrimary'>Fazenda</p>
+					</Link>
+				</div>
+
+				<div className='flex flex-col items-center gap-1'>
+					<Link
+						href='/trips/search?text=Chalé'
+						className='flex flex-col items-center hover:text-primary transition-all'
+					>
+						<Image
+							width={35}
+							height={35}
+							src='/cottage-icon.svg'
+							alt='Chalé'
+						/>
+						<p className='text-sm lg:text-base text-grayPrimary'>Chalé</p>
+					</Link>
+				</div>
+
+				<div className='flex flex-col items-center gap-1'>
+					<Link
+						href='/trips/search?text=pousada'
+						className='flex flex-col items-center hover:text-primary transition-all'
+					>
+						<Image
+							width={35}
+							height={35}
+							src='/inn-icon.svg'
+							alt='Pousada'
+						/>
+						<p className='text-sm lg:text-base text-grayPrimary'>Pousada</p>
+					</Link>
+				</div>
 			</div>
-		</section>
+		</div>
 	);
 };
 
