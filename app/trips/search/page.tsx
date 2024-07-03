afbbddc38b5d592ae85f8756b0a5cc5065a22fa8
Marlon Ferreira <ferreira.marlon@live.com>
2024-07-03 11:43:02 -0300

    fix: deploy

--- app/trips/search/page.tsx
+++ app/trips/search/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import TripItem from "@/app/_components/tripItem";
+import { Trip } from "@prisma/client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const Trips = () => {
-   const [trips, setTrips] = useState<any[]>([]);
+   const [trips, setTrips] = React.useState<Trip[]>([]);
 
    const searchParams = useSearchParams();
 
@@ -22,17 +23,19 @@ const Trips = () => {
    }, [searchParams]);
 
    return (
-      <section className="container mx-auto flex flex-col items-center p-5">
-         <h1 className="text-primaryDarker font-semibold text-xl">Viagens Encontradas</h1>
+      <div className="container mx-auto flex flex-col items-center lg:items-start p-5 lg:pt-10">
+         <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">Viagens Encontradas</h1>
+         <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
+            {trips.length > 0 ? "Listamos as melhores viagens pra você!" : "Não encontramos nada nos seus parâmetros! =("}
+         </h2>
 
-         <h2 className="text-grayPrimary font-medium mb-5">{trips?.length > 0 ? "Listamos as melhores hospedagens para você!" : "Nenhuma hospedagem encontrada!"}</h2>
-
-         <div className="flex flex-col gap-4">
+         <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
             {trips?.map((trip) => (
                <TripItem key={trip.id} trip={trip} />
             ))}
          </div>
-      </section>
+      </div>
    );
 };
+
 export default Trips;
